fae68504ff69efc9a30d52aae9fe7df8c4d52258
Hongpei Zheng <zhenghongpei@gmail.com>
2022-12-22 10:01:02 +0000

    Initial Commit

---
+++ Refuge/RefugeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RefugeApp.swift
+//  Refuge
+//
+//  Created by 弘培郑 on 22/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct RefugeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
